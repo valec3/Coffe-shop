8918e99019336b274aff23390be8431efcd8a520
valec3 <v1c70rotakmer@gmail.com>
2025-03-25 18:02:36 -0500

    Agregar componentes y estilos iniciales, incluyendo Header, Footer, y Welcome, junto con imágenes y configuración de navegación.

---
+++ src/constants/index.ts
@@ -0,0 +1,22 @@
+export const navOptions = [
+    {
+        id: 1,
+        title: "Inicio",
+        path: "/",
+    },
+    {
+        id: 2,
+        title: "Menú",
+        path: "/menu",
+    },
+    {
+        id: 3,
+        title: "Nosotros",
+        path: "#about",
+    },
+    {
+        id: 4,
+        title: "Galería",
+        path: "/gallery",
+    },
+];
