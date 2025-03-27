e98af3e003b5f860c70206c6659e89a93438030f
valec3 <v1c70rotakmer@gmail.com>
2025-03-27 12:53:41 -0500

    Agregar soporte para sitemap y archivo robots.txt; actualizar configuración de Astro y enlaces en el layout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: "https://ukumaricoffe.vercel.app",
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [sitemap()],
 });
